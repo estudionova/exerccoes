176598125f44c4f9b04beb0f5302b77b7e2491b4
Reinaldo Junior <rsljunior@live.com>
2022-03-31 17:49:55 -0300

    Bloco Finally

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,21 +9,60 @@ public class Program {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
+
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("Finally block executed");
+        }
+    }
+}
+/*
+    public static void method1() {
+        System.out.println("***METHOD1 START***");
+
+        method2();
+
+
+        System.out.println("***METHOD1 END***");
+
+    }
+
+ public static void method2() {
+
+        System.out.println("***METHOD2 START***");
         Scanner sc = new Scanner(System.in);
 
         try {
             String[] vect = sc.nextLine().split(" ");
             int position = sc.nextInt();
             System.out.println(vect[position]);
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (
+                ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid Position!");
-        } catch (InputMismatchException e) {
+//            e.printStackTrace();
+//            sc.next();
+        } catch (
+                InputMismatchException e) {
             System.out.println("Input error");
         }
-
-
-        System.out.println("END OF PROGRAM");
+        System.out.println("***METHOD2 END***");
 
         sc.close();
     }
 }
+
+
+    */
